749d363ad33fc76b755f3d71f3d042014832e204
Michal Muszynski <hi@geodev.me>
2025-12-24 15:33:36 +0100

    update config

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,19 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			pages: 'svelte-kit/output',
-			assets: 'svelte-kit/output',
+			pages: 'build',
+			assets: 'build',
 			fallback: 'index.html'
 		}),
 		paths: {
 			base: '/name-map-pl'
 		}
-	},
-	adapter: adapter()
+	}
 };
 
 export default config;
