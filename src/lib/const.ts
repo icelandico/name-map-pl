9f34d07321bf14993665289f355f903cb5e83a2b
Michal Muszynski <hi@geodev.me>
2025-12-26 20:12:52 +0100

    feat: add const with predefined options

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -20,3 +20,18 @@ export const getFilterSuffix = (value: string) => [
 		['-', ['length', ['get', 'name']], ['length', value]]
 	]
 ];
+
+export const PREDEFINED_OPTIONS = [
+	{ label: 'Starting with "Now"', value: 'now-' },
+	{ label: 'Starting with "Star"', value: 'star-' },
+	{ label: 'Starting with "Mał"', value: 'mał-' },
+	{ label: 'Ending with "-jmy"', value: '-jmy' },
+	{ label: 'Ending with "-szki"', value: '-szki' },
+	{ label: 'Ending with "-ork"', value: '-ork' },
+	{ label: 'Ending with "-ów"', value: '-ów' },
+	{ label: 'Ending with "-ówek"', value: '-ówek' },
+	{ label: 'Ending with "-chów"', value: '-chów' },
+	{ label: 'Ending with "-szew"', value: '-szew' },
+	{ label: 'Ending with "-góra"', value: '-góra' },
+	{ label: 'Ending with "-rsk"', value: '-rsk' }
+];
