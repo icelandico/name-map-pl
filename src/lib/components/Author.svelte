577a07fac301d89035489bc56e705b64ba4a60c6
Michal Muszynski <hi@geodev.me>
2025-12-27 15:49:13 +0100

    add target to anchor

--- src/lib/components/Author.svelte
+++ src/lib/components/Author.svelte
@@ -4,7 +4,10 @@
 
 <div class="p-1 bg-grey-500 rounded-md block w-fit ml-auto">
 	<span class="text-xs">{m.author()}</span>
-	<a class="text-xs cursor-pointer hover:text-blue-600" href="https://www.geodev.me"
-		>https://www.geodev.me</a
+	<a
+		class="text-xs cursor-pointer hover:text-blue-600"
+		href="https://www.geodev.me"
+		target="_blank"
+		rel="noopener noreferrer">https://www.geodev.me</a
 	>
 </div>
